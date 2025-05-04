3c45136616bb8de093892a3e5b94b923ef166884
24Marko42 <markoffka.kuznetsov@mail.ru>
2025-05-04 22:46:10 +0700

    ..

---
+++ zadanie1.py
@@ -0,0 +1,10 @@
+numbers = [15, 20, 8, 19, 25, 12]
+result = list(filter(lambda x: x < 5, numbers))
+result2 = list(map(lambda x: x / 2, numbers))
+result3 = list(map(lambda x: x / 2, filter(lambda x: x > 17, numbers)))
+
+
+numbers2 = range(10, 100)  # Все двузначные числа
+
+result4 = sum(map(lambda x: x**2, filter(lambda x: x % 9 == 0, numbers2)))
+print(result, result2, result3, result4)
